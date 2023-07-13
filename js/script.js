d35229e78624a163044c62edae1b8a51bc9d22de
Mariela1 <mariela.espinozam@gmail.com>
2023-07-13 16:06:29 -0500

    CRUD'Guardar+CreacionDeCollection

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js"
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js"
 import {auth} from './firebase.js';
 import {loginCheck} from './loginCheck.js';
+import { saveTask } from "./firebase.js";
 import './signupForm.js';
 import './logout.js';
 import './signinForm.js';
 import './googleLogin.js';
 
+
+const taskForm = document.querySelector('#task-form');
+
+
+taskForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    console.log('enviado')
+
+    const title = taskForm["task-title"];
+    const description = taskForm["task-description"];
+    //console.log(title.value, description.value) 
+     saveTask(title.value, description.value);
+     taskForm.reset();
+
+});
 
 
 onAuthStateChanged(auth, (user) => {
